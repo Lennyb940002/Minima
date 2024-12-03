00a2d3057233472b4d6c5c16ded60ff8be2adcb8
lenny bouche <lennybouche@outlook.com>
2024-12-03 16:42:14 +0100

    Description de la modification

--- src/components/Dashboard/DashboardCard.tsx
+++ src/components/Dashboard/DashboardCard.tsx
@@ -7,7 +7,7 @@ interface DashboardCardProps {
 
 export function DashboardCard({ title, Icon }: DashboardCardProps) {
   return (
-    <div className="bg-black border border-white px-6 py-8 rounded-lg transition-all duration-300 hover:bg-white group cursor-pointer top-10 rounded-[20px]" style={{ width: '380px', height: '260px' }}>
+    <div className="bg-black border border-white px-6 py-8 rounded-lg transition-all duration-300 hover:bg-white group cursor-pointer top-10 rounded-[25px]" style={{ width: '380px', height: '260px' }}>
       <div className="flex flex-col items-center gap-4">
         <Icon className="w-24 h-24 text-white group-hover:text-black" />
         <h3 className="text-white group-hover:text-black font-medium">{title}</h3>
